b7be105c14de48e0627b63703ebe23612dc047c7
Dennis-Lord <dennislord2001@gmail.com>
2023-02-04 23:20:15 +0000

    initial routes / endpoints

--- index.js
+++ index.js
@@ -2,8 +2,46 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+// routes / endpoints
+
+/**
+ * collection - medicalRecords
+ * docs - referred to as medical folders for some understanding sake 
+ */
+
+// get all records
+app.get('/records', (req, res) => {
+  res.send('GET req to records collection success')
+})
+
+// add new document (new user document) to records
+app.post('/records', (req, res) => {
+  res.send('POST req to add new doc success')
+})
+
+// get specified document (specified user document)
+app.get('/records/:id', (req, res) => {
+  console.log(req.params)
+  res.send('GET req to get specified doc success')
+})
+
+// add new data to specified document (specified user document)
+app.post('/records/:id', (req, res) => {
+  console.log(req.params)
+  res.send('GET req to add new data to specified doc success')
+})
+
+// update specified document (specified user document)
+app.put('/records/:id', (req, res) => {
+  console.log(req.params)
+  res.send('PUT req to update specified doc success')
+})
+
+// delete specified document (specified user document)
+app.delete('/records/:id', (req, res) => {
+  console.log(req.params)
+  res.send('PUT req to delete specified doc success')
 })
 
 app.listen(port, () => {
