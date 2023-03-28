eb3eb1a3499d538c540f58987a9bb22df6078e1d
Dennis-Lord <dennislord2001@gmail.com>
2023-03-28 16:36:33 +0000

    install firebase

--- src/routes.js
+++ src/routes.js
@@ -1,66 +1,84 @@
 import { records } from "./model";
 
+let myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
 const routes = (app) => {
     // get all records
-    app.route('/records')
-    .get((req, res) => {
-        records.find({}, (err, doc) => {
-            if(err) return console.log(err);
-            res.json(doc)
-        })
-    })
-    // post record
-    app.route('/records')
-    .post((req, res) => {
-        let r = new records(req.body)
+    // app.route('/records')
+    // .get((_req, res) => {
+    //     records.find({}, (err, doc) => {
+    //         if(err) return console.log(err);
+    //         res.json(doc)
+    //     })
+    // })
 
-        r.save((err, doc) => {
-            if(err) return console.log(err);
-            res.json(doc)
-        })
-    })
+    // create new collection, and document
+    // app.route('/records/create')
+    // .post((req, res) => {
+    //     let r = new records(req.body)
 
-    app.route('/records/:userId')
-    .get((req, res) => {
-        records.findById(req.params.userId, (err, doc) => {
-            if(err){
-                res.send(err);
-            }
-            res.json(doc);
-        });
-    })
+    //     r.save((err, doc) => {
+    //         if(err) return console.log(err);
+    //         res.json(doc)
+    //     })
+    // })
 
-    // put request to update specified document
-    .put((req, res) => {
-        records.findOneAndUpdate({ _id: req.params.userId},req.body, { new: true, useFindAndModify: false }, (err, doc) => {
-            if(err){
-                res.send(err);
-            }
-            res.json(doc);
-        });
-    })
+    // get specific doc by id
+    // app.route('/records/:_id')
+    // .get((req, res) => {
+    //     records.findById(req.params._id, (err, doc) => {
+    //         if(err){
+    //             res.send(err);
+    //         }
+    //         res.json(doc);
+    //     });
+    // })
+
+    // update specific document by id
+    // .put((req, res) => {
+    //     records.findOneAndUpdate({ _id: req.params._id},req.body, { new: true, useFindAndModify: false }, (err, doc) => {
+    //         if(err){
+    //             res.send(err);
+    //         }
+    //         res.json(doc);
+    //     });
+    // })
 
     // delete request
-    .delete((req, res) => {
-        records.findOneAndRemove({ _id: req.params.userId}, (err, doc) => {
-            if(err){
-                res.send(err);
-            }
-            res.json({message: 'Successfully deleted medical record'});
-        })
-    })
+    // .delete((req, res) => {
+    //     records.findOneAndRemove({ _id: req.params._id}, (err, doc) => {
+    //         if(err){
+    //             res.send(err);
+    //         }
+    //         res.json({message: 'Successfully deleted medical record'});
+    //     })
+    // })
 
     // backup specific record to firestore
-    app.route('/backup/:userId')
+    app.route('/backup/:_id')
     .post((req, res) => {
-        let document;
-        records.findById(req.params.userId, (err, doc) => {
+        // find document to backup
+        records.findById(req.params._id, async (err, doc) => {
             if(err){
                 res.send(err);
             }
-            document = json(doc);
-        });
 
+            // fetch request options 
+            const requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: JSON.stringify(doc), // document being backed up to firestore
+                redirect: 'follow'
+            };
+
+            const d = await fetch("http://127.0.0.1:5001/patient-app-api-de1a9/us-central1/app/create", requestOptions)
+            .then((response) => {
+                return response
+            })
+
+            res.send(d.status)
+        });
     })
 };
 
